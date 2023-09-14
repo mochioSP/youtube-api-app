1ab235a9f3effb2eea1a4efa96ba0f765ef45f70
mochioSP <keeee50114@gmail.com>
2023-09-14 23:57:41 +0900

    はすきー以外の犬のボタン設置

--- src/components/organism/layout/SideBar.tsx
+++ src/components/organism/layout/SideBar.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { useState } from "react";
 
 import "../../../style/sidebar.css";
 
--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -6,7 +6,9 @@ import { DogChoiceButton } from "../atom/DogChoiceButton";
 
 export const Layout = () => {
   const navigate = useNavigate();
+  const onClickShibaVideo = () => navigate("/");
   const onClickHuskyVideo = () => navigate("/husky");
+  const onClickToypooVideo = () => navigate("/poodle");
 
   return (
     <>
@@ -15,12 +17,19 @@ export const Layout = () => {
         <SideBar />
         <div className="main">
           <Search />
-          <div
-            className="huskyButton"
-            style={{ textAlign: "center", marginTop: "45px" }}
-          >
+          <div className="shibaButton" style={{ marginLeft: "60px" }}>
+            <DogChoiceButton onClick={onClickShibaVideo}>
+              しばいぬ
+            </DogChoiceButton>
+          </div>
+          <div className="huskyButton" style={{ marginLeft: "60px" }}>
             <DogChoiceButton onClick={onClickHuskyVideo}>
               はすきー
+            </DogChoiceButton>
+          </div>
+          <div className="toypooButton" style={{ marginLeft: "60px" }}>
+            <DogChoiceButton onClick={onClickToypooVideo}>
+              といぷー
             </DogChoiceButton>
           </div>
           <div className="video-Area">
